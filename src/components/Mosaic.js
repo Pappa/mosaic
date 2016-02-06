6716c828c258e3bf26d14648632fa29e7a3afae7
Robert Hill <dr.pappa@gmail.com>
2016-02-06 11:16:24 +0000

    Mosaic component

---
+++ src/components/Mosaic.js
@@ -0,0 +1,14 @@
+"use strict";
+var React = require('react');
+
+var Mosaic = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<p>dfsdfsd</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Mosaic;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
 "use strict";
+var React = require('react'),
+	ReactDOM = require('react-dom'),
+	Mosaic = require('./components/Mosaic');
 
-var React = require('react');
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Mosaic />
+			</div>
+		);
+	}
+});
+
+function render() {
+	ReactDOM.render(
+		<App />, 
+		document.getElementById('app')
+	);
+}
+
+document.addEventListener('DOMContentLoaded', render, false);
